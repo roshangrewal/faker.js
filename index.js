02cd21c13b3b0040bceb293784af02339bda54c9
roshangrewal <roshangrewal@hotmail.com>
2020-03-17 23:10:26 +0530

    Merged Aniket Work

--- index.js
+++ index.js
@@ -2,4 +2,12 @@
 var Faker = require('./lib');
 var faker = new Faker({ locales: require('./lib/locales') });
 module['exports'] = faker;
-console.log(faker.itcompanies.companies());
+console.log('------------------------------ IT Companies Section ---------------------------------');
+console.log('Company Name: ' + faker.itcompanies.companies());
+console.log('Job Title: ' + faker.itcompanies.jobTitle());
+console.log('Job Description: ' + faker.itcompanies.jobDescription());
+console.log(' ');
+
+console.log('--------------------------- Internet Provider Section -------------------------------');
+console.log('Internet Provider Name: ' + faker.internetProvider.providers());
+console.log('Internet Provider Description: ' + faker.internetProvider.providerDescription());
